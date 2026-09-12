9fcac832a450eea4a8199e4fe8382be9f93d090a
Ariel Muhamad Sava <arielmuhamadsava@mail.ugm.ac.id>
2026-09-13 00:37:12 +0700

    program esp32 pertemuan 2

---
+++ Pertemuan_1/src/main.cpp
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+
+const int LedPin = 16;
+
+// put function declarations here:
+
+void setup() {
+  // put your setup code here, to run once:
+  // Mengatur pin LED sebagai OUTPUT
+  pinMode(LedPin, OUTPUT);
+  
+  // Memulai komunikasi serial untuk debugging
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LedPin, HIGH);  // Menyalakan LED
+  Serial.println("LED Menyala");
+  delay(3000);                 // Menunggu 3 detik
+  
+  digitalWrite(LedPin, LOW);   // Mematikan LED
+  Serial.println("LED Mati");
+  delay(3000);                 // Menunggu 3 detik
+}
+
---
+++ pertemuan_2_reading_ADC_dan_konversi/src/main.cpp
@@ -0,0 +1,45 @@
+#include <Arduino.h>
+
+#define POT_PIN 34
+
+// Spesifikasi ESP32
+#define ADC_RESOLUTION 4095.0  // ADC 12-bit (0 - 4095)
+#define V_REF 3.3              // Tegangan referensi 3.3V
+#define R_MAX 10000.0          // Hambatan maksimal potensiometer 10k Ohm (10000 Ohm)
+
+void setup() {
+  // Inisialisasi Serial Monitor dengan baudrate 115200
+  Serial.begin(115200);
+  
+  // Konfigurasi pin potensiometer sebagai INPUT
+  pinMode(POT_PIN, INPUT);
+  
+  Serial.println("--- Mulai Pembacaan Potensiometer ESP32 ---");
+}
+
+void loop() {
+  // Baca nilai mentah (Raw) ADC dari pin GPIO 34
+  int rawADC = analogRead(POT_PIN);
+
+  // Hitung nilai konversi Tegangan (Voltage) dan Hambatan (Resistance)
+  // Rumus Tegangan = (Raw ADC / 4095) * 3.3V
+  float tegangan = (rawADC / ADC_RESOLUTION) * V_REF;
+  
+  // Rumus Hambatan = (Raw ADC / 4095) * 10000 Ohm
+  float hambatan = (rawADC / ADC_RESOLUTION) * R_MAX;
+
+  // Tampilkan data ke Serial Monitor secara rapi
+  Serial.print("Raw ADC: ");
+  Serial.print(rawADC);
+  
+  Serial.print(" | Tegangan: ");
+  Serial.print(tegangan, 2); // Menampilkan 2 angka di belakang koma
+  Serial.print(" V");
+  
+  Serial.print(" | Hambatan: ");
+  Serial.print(hambatan, 0); // Menampilkan bilangan bulat
+  Serial.println(" Ohm");
+
+  // Tambahkan delay setiap siklus pembacaan (5 detik sekali)
+  delay(5000);
+}
